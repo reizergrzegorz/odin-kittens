7909d4a4b5766e7943e239811b8afd0ea4014009
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-09 10:32:50 +0200

    add methods to kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,55 @@
 class KittensController < ApplicationController
-end
+    before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @kittens = Kitten.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @kitten = Kitten.new
+    end
+  
+    def create
+      @kitten = Kitten.new(kitten_params)
+      if @kitten.save
+        flash[:notice] = "Kitten created successfully!"
+        redirect_to @kitten
+      else
+        flash[:alert] = "There was an error creating the kitten."
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @kitten.update(kitten_params)
+        flash[:notice] = "Kitten updated successfully!"
+        redirect_to @kitten
+      else
+        flash[:alert] = "There was an error updating the kitten."
+        render :edit
+      end
+    end
+  
+    def destroy
+      @kitten.destroy
+      flash[:notice] = "Kitten deleted successfully!"
+      redirect_to kittens_path
+    end
+  
+    private
+  
+    def set_kitten
+      @kitten = Kitten.find(params[:id])
+    end
+  
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+  end
+  
